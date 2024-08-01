7ba8babfa99af5a4759ac4cead02f00496a26ff8
NarasimhaRebel <129524359+NarasimhaRebel@users.noreply.github.com>
2024-08-01 12:26:03 +0530

    Create data_type.py

---
+++ data_type.py
@@ -0,0 +1,34 @@
+
+a=5 
+print(a,type(a))
+
+b=7.8
+print(b,type(b))
+
+c='hi'
+d="hey"
+e='''hello'''
+f="""python"""
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(c,d,e,f)
+print(c)
+print(d)
+
+name=input()
+age=int(input())
+qualification=input()
+percentage=float(input())
+print(name,age,qualification,percentage)
+print('your name is' ,name)
+print('your age is' ,age)
+print('your qualification  is',qualification)
+print('your percentage is' ,percentage)
+
+
+name=input()
+age=int(input())
+qualification=input()
+print(f'your name is{name},your age is{age},your qualification is {qualification},your percentage is{percentage}')
