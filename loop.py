4b948f018ff13fc27d9561a189004782bf5fdcf4
NarasimhaRebel <129524359+NarasimhaRebel@users.noreply.github.com>
2024-08-01 12:23:10 +0530

    Create loop.py

---
+++ loop.py
@@ -0,0 +1,14 @@
+
+a=input()
+print(a, 'is a language')
+ 
+a=('enter the course name:')
+print(a, 'is a programming language')
+
+
+b=float(input('enter your weight:'))
+print('your weight is' ,b)
+
+a=float(input())
+b=float((input()))
+print(a+b)
